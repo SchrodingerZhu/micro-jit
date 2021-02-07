4d669007cf85b7ca5caa6367a76b105fb9f87a74
SchrodingerZhu <i@zhuyi.fan>
2021-02-07 23:23:15 +0800

    show how to throw exception

--- src/main/java/micro/nodes/MicroIdentNode.java
+++ src/main/java/micro/nodes/MicroIdentNode.java
@@ -1,14 +1,20 @@
 package micro.nodes;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
 
 public class MicroIdentNode extends MicroExprNode {
     private String name;
-
+    @CompilerDirectives.CompilationFinal boolean noSuchVar = false;
     @Override
     public Object executeGeneric(VirtualFrame frame) {
        var slot = frame.getFrameDescriptor().findFrameSlot(name);
+       if (slot == null && !noSuchVar) {
+           CompilerDirectives.transferToInterpreter();
+           noSuchVar = true;
+           throw new RuntimeException("no such variable");
+       }
        return frame.getValue(slot);
     }    
 
